63bdf359e39262bec046a75f927c64a5a7fdf041
Jonathan Birge <birge@mit.edu>
2023-05-10 01:03:06 +0000

    filter.py uses stdin

--- filter.py
+++ filter.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
 
-def filter_ascii_lines(file_in, file_out):
-    with open(file_in, 'r') as fin, open(file_out, 'w') as fout:
-        for line in fin:
-            if not contains_non_ascii(line):
-                fout.write(line)
+import sys
+
+def filter_ascii_lines():
+    for line in sys.stdin:
+        if len(line) >= 3 and not contains_non_ascii(line):
+            sys.stdout.write(line)
 
 def contains_non_ascii(s):
     return any(ord(c) > 127 for c in s)
 
-def main():
-    file_in = input("Enter the name of the input file: ")
-    file_out = input("Enter the name of the output file: ")
-    filter_ascii_lines(file_in, file_out)
-    print("Done!")
-
 if __name__ == "__main__":
-    main()
+    filter_ascii_lines()
