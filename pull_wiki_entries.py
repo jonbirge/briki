15e444d11d5df7e3edd138ca25432247fda2bdbc
Jonathan Birge <birge@mit.edu>
2023-05-12 02:32:47 +0000

    sorta working integrated validate and pull

--- pull_wiki_entries.py
+++ pull_wiki_entries.py
@@ -15,46 +15,46 @@ def add_space_after_period(text):
     # followed by a non-space character or a non-digit character
     return re.sub(r'\.(?=[^\s\d])', '. ', text)
 
+def is_good_title(title):
+    # Check if the title is a good title
+    return (len(title) > 2 and 
+            not title.startswith("Talk:") and
+            not title.startswith("Help:"))
 
-### Create SQLite database and tables, if needed...
+def filter_titles(titles):
+    # Filter out titles that are not good titles
+    titles_filtered = []
+    for line in titles:
+        if is_good_title(line):
+            titles_filtered.append(line)
+    return titles_filtered
 
-# Connect to SQLite database (or create it if it doesn't exist)
-conn = sqlite3.connect('bliki.db')
+def validated_titles(raw_title):
+    # Check raw_title and return a (potentially empty) list of validated titles
+    # TODO: check for foreign characters or short names
+    titles = []
+    page = wiki.page(raw_title)
+    if page.exists():
+        if "Category:All disambiguation pages" in page.categories:
+            print("%s links to %s, a disambiguation page." % (raw_title, page.title))
+            for link in page.links:
+                titles.append(link)
+            titles = filter_titles(titles)
+        else:
+            print("%s links to %s." % (raw_title, page.title))
+            titles.append(page.title)
+    return titles
 
-# Create a cursor object
-cursor = conn.cursor()
-
-# Check to see if articles table exists and if it doesn't create it...
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS articles(
-        id INTEGER PRIMARY KEY,
-        title TEXT NOT NULL,
-        date_update TEXT,
-        contents TEXT,
-        see_also TEXT)
-''')
-               
-
-### Write each element of dict_titles to the articles table if it doesn't already exist
-
-# Read each line of dict_test_valid into an array
-with open('dict_valid') as f:
-    dict_titles = f.readlines()
-
-# Run through all titles and pull the contents from Wikipedia
-wiki = wikipediaapi.Wikipedia('en')
-for title in dict_titles:
-    # Remove the newline character from the end of the title
-    title = title.rstrip('\n')
+def pull_valid_article(title):
+    # Add a valid article to the articles table from the Wikipedia API
 
     # Check to see if the title is already in the articles table
+    # TODO: Also check date_update to see if it is out of date
     cursor.execute('SELECT * FROM articles WHERE title=?', (title,))
     row = cursor.fetchone()
-    if row is None:
-        # If the title is not in the articles table, add it
-        print("Adding %s to articles table" % title)
+    if row is None:  # We don't have this one yet.
+        print("Pulling %s into to database..." % title)
         page = wiki.page(title)
-
         contents = add_space_after_period(page.summary)
 
         # Get the current data and time
@@ -70,12 +70,63 @@ for title in dict_titles:
         cursor.execute('''
             INSERT INTO articles(title, date_update, contents, see_also)
             VALUES(?, ?, ?, ?)''', (title, date_update, contents, see_also))
+        conn.commit()
     else:
-        print("%s is already in the articles table" % title)
+        print("Skipping %s since it's already in the database.", title)
+
+def add_title(title):
+    # Add all articles matching title to the articles table,
+    # using wiki as the Wiki object and cursor as the SQL cursor.
+    
+    print("Checking new title %s..." % title)
+    # If the title is not in the articles table, check to see if it's a valid title
+    valid_title_list = validated_titles(title)
+    print("Valid titles: %s" % valid_title_list)
+
+    # Check to see if it's a valid article or redirected
+    if len(valid_title_list) > 0:
+        if len(valid_title_list) == 1:
+            # If it's a valid article, pull it from Wikipedia
+            pull_valid_article(valid_title_list[0])
+        else:
+            # Run add_title on each title in valid_titles
+            for valid_title in valid_title_list:
+                add_title(valid_title)
+    else:
+        print("No valid titles found for %s." % title)
+
+
+### Create SQLite database and tables, if needed...
+
+# Connect to SQLite database (or create it if it doesn't exist)
+conn = sqlite3.connect('bliki.db')
+cursor = conn.cursor()
+
+# Check to see if articles table exists and if it doesn't create it...
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS articles(
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        date_update TEXT,
+        contents TEXT,
+        see_also TEXT)
+''')
+               
+
+### Update the database of definitions for dict_test...
+
+# Read each line of dict_test into an array
+with open('dict_test') as f:
+    dict = f.readlines()
+
+# Run through all titles and pull the contents from Wikipedia
+wiki = wikipediaapi.Wikipedia('en')
+for raw_title in dict:
+    # Remove the newline character from the end of the title
+    raw_title = raw_title.rstrip('\n')
+
+    # Add the title to the articles table
+    add_title(raw_title)
         
-
-# Commit changes to the database
-conn.commit()
-
-# Close the database connection
+# Close the database
 conn.close()
