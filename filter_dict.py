fc4fd9906c693ebb23591ddf53389f01a53b455c
Jonathan Birge <birge@mit.edu>
2023-05-14 00:41:26 +0000

    remove possesive forms from dict

--- filter_dict.py
+++ filter_dict.py
@@ -4,15 +4,24 @@
 
 import sys
 
-def filter_lines(lines_in):
-    lines_out = []
-    for line in lines_in:
-        if len(line) > 3 and not contains_non_ascii(line):
-            lines_out.append(line)
-    return lines_out
+def is_good_line(the_line):
+    the_line = the_line.strip()
+    return (len(the_line) > 3 and
+            not contains_non_ascii(the_line) and
+            not the_line.endswith("'s"))
 
 def contains_non_ascii(s):
     return any(ord(c) > 127 for c in s)
+
+def old_filter_lines(lines):
+    new_lines = []
+    for line in lines:
+        if is_good_line(line):
+            new_lines.append(line)
+    return new_lines
+
+def filter_lines(lines):
+    return [line for line in lines if is_good_line(line)]
 
 if __name__ == "__main__":
     lines_in = sys.stdin.readlines()
