c67215057e548f8c723f168a1ba7b1daa5531135
Jonathan Birge <birge@mit.edu>
2023-05-14 14:46:32 -0400

    fixed bug in toc fix utility

--- toc_table_from_log.py
+++ toc_table_from_log.py
@@ -31,12 +31,17 @@ cursor.execute(f'''
     CREATE TABLE IF NOT EXISTS {TOC_TABLE}(
         id INTEGER PRIMARY KEY,
         term TEXT NOT NULL,
-        refs TEXT)
+        refs TEXT NOT NULL)
 ''')
 
 # Read log from the standard input, one line at a time.
 read_next = False
+n = 0
 for log_line in sys.stdin:
+    n += 1
+    if n % 1000 == 0:
+        print(f"Read {n} lines...", file=sys.stderr)
+
     # Remove leading and trailing whitespace.
     log_line = log_line.strip()
 
@@ -63,33 +68,35 @@ for log_line in sys.stdin:
 
     # Add reference(s) to database...
     if len(titles) > 0:
-        # Add a row to the "toc" table with the line just read as the "term" column.
-        # The "references" column should be a JSON-encoded list of the titles found.
         print(f"Adding {term} row to toc table...")
+        titles_json = json.dumps(titles)
         cursor.execute(f"INSERT INTO {TOC_TABLE} (term, refs) VALUES (?, ?);",
-                        (term, json.dumps(titles)))
+                        (term, titles_json))
         
         # Now, go through each title in titles and find the corresponding row in the "articles" table.
         # Add the title to the "references" column of that row.
-        for title in titles:
-            cursor.execute(f"SELECT * FROM {ARTICLE_TABLE} WHERE title = ?;", (title,))
-            row = cursor.fetchone()
-            if row is None:
-                print("Error: Could not find row for title '%s'" % title)
-            else:
-                # Check to see if row[4] is None. If it is, set refs_in to an empty list.
-                if row[4] == "":
-                    refs_in = []
-                else:
-                    # Decode the JSON-encoded list of references.
-                    refs_in = json.loads(row[4])
-                # Add the line just read to the list of references.
-                refs_in.append(term)
-                # Remove duplicates from the list of references.
-                refs_in = list(set(refs_in))
-                # Update the row with the new list of references.
-                cursor.execute(f"UPDATE {ARTICLE_TABLE} SET see_also = ? WHERE title = ?",
-                            (json.dumps(refs_in), title))
+        # for title in titles:
+        #     cursor.execute(f"SELECT * FROM {ARTICLE_TABLE} WHERE title = ?;", (title,))
+        #     row = cursor.fetchone()
+        #     if row is None:
+        #         print("Error: Could not find row for title '%s'" % title)
+        #     else:
+        #         # Check to see if row[4] is None. If it is, set refs_in to an empty list.
+        #         if row[4] == "":
+        #             refs_in = []
+        #         else:
+        #             # Decode the JSON-encoded list of references.
+        #             refs_in = json.loads(row[4])
+        #         # Add the line just read to the list of references.
+        #         refs_in.append(term)
+        #         # Remove duplicates from the list of references.
+        #         refs_in = list(set(refs_in))
+        #         # Update the row with the new list of references.
+        #         cursor.execute(f"UPDATE {ARTICLE_TABLE} SET see_also = ? WHERE title = ?",
+        #                     (json.dumps(refs_in), title))
+
+# Commit the changes to the database.
+conn.commit()
 
 # Close the connections to both databases.
 conn.close()
